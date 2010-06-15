37617f9265fe289c442da193e62859bcd930dcaa
Alexis Agahi <alag@users.sf.net>
2010-06-15 18:51:14 +0200

    Add default 1.5 project

---
+++ src/main/java/com/treegger/android/im/AndroIM.java
@@ -0,0 +1,13 @@
+package com.treegger.android.im;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroIM extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
