c2a73be01facfe5a67f4856a88e0d70de5012a1e
Alexis Agahi <alag@users.sf.net>
2010-06-17 00:20:13 +0200

    Clean up and finish activity on cancel

--- src/main/java/com/treegger/android/im/AccountForm.java
+++ src/main/java/com/treegger/android/im/AccountForm.java
@@ -3,7 +3,9 @@ package com.treegger.android.im;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class AccountForm extends Activity {
@@ -18,11 +20,19 @@ public class AccountForm extends Activity {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.accountform);
        
-       Spinner spinner = (Spinner) findViewById(R.id.spinner);
+       Spinner spinner = (Spinner) findViewById(R.id.input_socialnetwork);
        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item, SOCIAL_NETWORKS );
        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
        spinner.setAdapter(adapter);
 
+       
+       
+       Button cancel = (Button)findViewById(R.id.button_cancel);
+       cancel.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               finish();
+           }
+       });
     }
 
     
